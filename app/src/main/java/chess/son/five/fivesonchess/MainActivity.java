b5f1564a49687734c857681738ee873cf4b26e8f
Wang Zhehong <zhehong.kevin.wang@hotmail.com>
2018-04-28 14:07:39 -0500

    234

--- app/src/main/java/chess/son/five/fivesonchess/MainActivity.java
+++ app/src/main/java/chess/son/five/fivesonchess/MainActivity.java
@@ -12,10 +12,30 @@ import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    /** */
+    private static final String API_KEY = "0d5fe03e9e9d465dd6da5db4194dbe57";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestQueue = Volley.newRequestQueue(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         final Game game = (Game) findViewById(R.id.qwe123);
@@ -35,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 game.init();
+                startAPICall();
             }
         });
         /*
@@ -81,5 +102,35 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "http://api.openweathermap.org/data/2.5/weather?zip=61820,us&appid="
+                            + API_KEY,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d("nihao", response.toString(2));
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e("nihao", error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String weather(final String json) {
+        JsonParser parser = new JsonParser();
+        JsonOject result
     }
 }
---
+++ app/src/main/java/chess/son/five/fivesonchess/Welcome.java
@@ -0,0 +1,6 @@
+package chess.son.five.fivesonchess;
+
+import android.app.Activity;
+
+public class Welcome extends Activity {
+}
